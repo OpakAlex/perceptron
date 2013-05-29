4e337d6c4545bbd1cbf0570500156508b1ee3602
Alex Opak <opak.alexandr@gmail.com>
2013-05-29 19:15:41 +0300

    add neuron with activator

--- neuron.rb
+++ neuron.rb
@@ -1,18 +1,18 @@
 #http://computing.dcu.ie/~humphrys/Notes/Neural/single.neural.html
 
 class Perceptron
-  
+
   attr_accessor  :threshold, :inputs
-  
+
   def initialize inputs, weights
     @inputs = inputs
     @neurons = weights.map do |w|
       Neuron.new(w)
     end
   end
-  
+
   def output
-      val = @inputs.zip(@neurons.map(&:w)).inject(0) { |total, i| 
+      val = @inputs.zip(@neurons.map(&:w)).inject(0) { |total, i|
         total + i[0] * i[1]
       }
       val < @threshold ? false: true
@@ -20,17 +20,56 @@ class Perceptron
 end
 
 class Neuron
-  attr_accessor :w 
-  
-  def initialize w
-    @w = w
+  attr_accessor :w, :s
+
+  def initialize count
+    @s = 50
+    @count = count
+    @w = []
   end
-  
+
+  def transfer(x)
+    activator(adder(x))
+  end
+
+
+  #Сумматор
+  #x - входной вектор
+  #return - невзвешенная сумма nec (биас не используется)
+  def adder(x)
+    x.each.inject(0){ |total, i| nec + x[i] * @w[i] }
+  end
+
+  def init_weights n
+    rand = Random.new
+    @count.times do |i|
+      @w[i] = rand(n)
+    end
+  end
+
+  #Модификация весов синапсов для обучения
+  #v - скорость обучения
+  #d - разница между выходом нейрона и нужным выходом
+  #x - входной вектор
+  def change_weights(v, d, x)
+    @count.times do |i|
+      @w[i] += v*d*x[i]
+    end
+  end
+
+  #Нелинейный преобразователь или функция активации,
+  #в данном случае - жесткая пороговая функция,
+  #имеющая область значений {0;1}
+  # nec - выход сумматора
+
+  def activator(nec)
+    nec >= s ?  1 : 0
+  end
+
 end
 
-#input vecrors:
-# 1001, 1011, 0001, 0011
+# Neuron.new(4).init_weights(10)
 
-perc = Perceptron.new [1, 1, 1, 0], [1, 0, 1, 1]
-perc.threshold = 2
-puts perc.output
+# perc = Perceptron.new [1, 1, 1, 0], [1, 0, 1, 1]
+# perc.threshold = 2
+# puts perc.output
